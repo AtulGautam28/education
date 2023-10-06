c5e1cb7413b7cd3bf6f90a89bdbcaa17ae008d02
Deepshikha yadav <deep.cubixsys@gmail.com>
2023-10-06 15:46:18 +0530

    updatedata

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -18,7 +18,7 @@
                             <th width="20%">{{ $websiteLang->where('lang_key','email')->first()->custom_text }}</th>
                             <th width="15%">{{ $websiteLang->where('lang_key','phone')->first()->custom_text }}</th>
                             <th width="10%">{{ $websiteLang->where('lang_key','photo')->first()->custom_text }}</th>
-                            <th width="5%">{{ $websiteLang->where('lang_key','property')->first()->custom_text }}</th>
+                            <!-- <th width="5%">{{ $websiteLang->where('lang_key','property')->first()->custom_text }}</th> -->
                             <th width="10%">{{ $websiteLang->where('lang_key','status')->first()->custom_text }}</th>
                             <th width="15%">{{ $websiteLang->where('lang_key','action')->first()->custom_text }}</th>
                         </tr>
@@ -32,7 +32,7 @@
                             <td>{{ $item->phone }}</td>
                             <td> <img src="{{ $item->image ? url($item->image) : "" }}" width="80px">
                             </td>
-                            <td>{{ $item->properties->count() }}</td>
+                            <!-- <td>{{ $item->properties->count() }}</td> -->
                             <td>
                                 @if ($item->status==1)
                                 <a href="" onclick="userStatus({{ $item->id }})"><input type="checkbox" checked data-toggle="toggle" data-on="{{ $websiteLang->where('lang_key','active')->first()->custom_text }}" data-off="{{ $websiteLang->where('lang_key','inactive')->first()->custom_text }}" data-onstyle="success" data-offstyle="danger"></a>
